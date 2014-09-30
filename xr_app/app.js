b50276a7e609014499c5c4d74212bbbd854a630d
ralphreid <beresfordjunior@me.com>
2014-09-30 19:21:59 +0200

    handling callback or outcome of the request

--- xr_app/app.js
+++ xr_app/app.js
@@ -2,11 +2,23 @@
 
   return {
     events: {
-      'app.activated':'showInfo'
+      'app.activated':'getInfo'
+    },
+
+    requests: {
+      relatedInfoSettings: function(id){
+        return {
+          url: '/api/v2/users/' + id + '/related.json',
+          type: 'GET',
+          dataType: 'json'
+        };
+      }
+
     },
 
     getInfo: function() {
       var id = this.ticket().requester().id();
+      var request = this.ajax('relatedInfoSettings', id);
     },
 
     showInfo: function() {
